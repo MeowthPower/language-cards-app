764ff30eb59cbcd925f0864ebb9457de8d661a54
Phillip Witkin <pwitkin273@gmail.com>
2015-05-26 09:56:15 -0400

    translation table fixed, again

--- language-app/db/migrate/20150526134128_add_translations.rb
+++ language-app/db/migrate/20150526134128_add_translations.rb
@@ -1,6 +1,6 @@
 class AddTranslations < ActiveRecord::Migration
   def change
-    create_table :tranlations do |t|
+    create_table :translations do |t|
 
       t.string :translation
       t.string :language
