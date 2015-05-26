ddafd2c1c9e177dddb8977f490e8ce87eefaa817
Liz Fonseca <lfonsec04@gmail.com>
2015-05-26 09:47:46 -0400

    added cards migration

---
+++ language-app/db/migrate/20150526134218_addcards.rb
@@ -0,0 +1,9 @@
+class Addcards < ActiveRecord::Migration
+  def change
+      create_table :cards do |t|
+        t.string :english_phrase
+        t.string :explanation
+
+        t.timestamps null: false
+  end
+end
