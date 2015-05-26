48a9d6b78c689e41609d89e96c849679247acdf8
Daniel Tomic <tom_danny2003@yahoo.com>
2015-05-26 09:53:12 -0400

    add users

---
+++ language-app/db/migrate/20150526134238_add_users.rb
@@ -0,0 +1,10 @@
+class AddUsers < ActiveRecord::Migration
+  def change
+      create_table :users do |t|
+      t.string :username
+      t.string :password
+      t.boolean :admin, default: false
+      t.timestamps null: false
+    end
+  end
+end
