28e5b66f2de2bfea373a25749b0e93c7c62bc001
Liz Fonseca <lfonsec04@gmail.com>
2015-05-26 19:23:06 -0400

    favorites controller logic added

--- language-app/app/controllers/api/favorites_controller.rb
+++ language-app/app/controllers/api/favorites_controller.rb
@@ -1,15 +1,35 @@
 module Api
   class FavoritesController < ApplicationController
     def index
-      
+      favorites = Favorite.find_by user_id: current_user[:id]
+      render json: favorites.to_json( include:{ 
+        cards:{
+          include:{
+            translations:{
+              include:{
+                user:{
+                  only: [:username]
+                }
+              }
+            },
+            categories:{
+             only:[:category_name]
+            }
+          }
+        })
     end
 
     def create
-      
+      new_favorite = Favorite.new(params[:favorite])
+      new_favorite.save
+      render json: new_favorite
     end
 
     def destroy
-      
+      favorite = Favorite.find(params[:id])
+      favorite.destroy
+      # need to figure out what to render
+      render json: => 'ok'
     end
   end
 end
