588ee86a947238f7ecc96a2836aad304ba2a357f
anna rankin <anna@annarankin.com>
2015-05-26 11:34:28 -0400

    added authentication

--- language-app/app/controllers/application_controller.rb
+++ language-app/app/controllers/application_controller.rb
@@ -2,4 +2,23 @@ class ApplicationController < ActionController::Base
   # Prevent CSRF attacks by raising an exception.
   # For APIs, you may want to use :null_session instead.
   protect_from_forgery with: :exception
+
+  def current_user
+    if session[:user_id]
+      @current_user ||= User.find(session[:user_id])
+    else
+      false
+    end
+  end
+
+  def logged_in?
+    current_user.present?
+  end
+
+  def authenticate
+    unless current_user
+      redirect_to root
+    end
+  end
+
 end
--- language-app/app/controllers/logins_controller.rb
+++ language-app/app/controllers/logins_controller.rb
@@ -1,13 +1,36 @@
 class LoginsController < ApplicationController
 
   def create
+    # looking for user in DB
+
+    user = User.find_by(username: params[:username])
+
+    if user && user.authenticate(params[:password])
+
+      session[:user_id] = user.id
+      redirect_to root_path
+
+    else
+      @message = "NO WRONG TRY AGIN"
+      render :new
+    end
+
+
   end
 
   def new
+    @current_user = current_user
+    render :new
+    # @current_user
+    # Demonstrating a possible use of current_user method - brings logged-in users bak to homepage if they try to log in again.
+    # if current_user
+    #   redirect_to root_path
+    # end
   end
 
   def destroy
-
+    session[:user_id] = nil
+    redirect_to root_path
   end
 
 end
