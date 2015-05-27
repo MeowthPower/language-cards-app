6b96157d0ff169f68c72054466b1971916e1b560
Liz Fonseca <lfonsec04@gmail.com>
2015-05-26 21:18:15 -0400

    implemented basic form to add cards

--- language-app/app/assets/javascripts/backbone/router/router.js
+++ language-app/app/assets/javascripts/backbone/router/router.js
@@ -2,7 +2,8 @@ var Meowth = Meowth || { Models: {}, Collections: {}, Views: {}, Routers: {} };
 
 Meowth.Routers.AppRouter = Backbone.Router.extend({
   routes :{
-    '': 'index'
+    '': 'index',
+    'addcard': 'addCard'
   },
 
   index: function(){
@@ -10,5 +11,12 @@ Meowth.Routers.AppRouter = Backbone.Router.extend({
     var recentCollection = new Meowth.Collections.RecentCards();
     // instantiate new collectionView
     var cardListView = new Meowth.Views.CardListView({collection: recentCollection, el: $('tbody')});
+  },
+
+  addCard: function(){
+    // instantiate new view
+    var newCard = new Meowth.Views.NewCard()
+    // renders view template
+    newCard.render()
   }
 })
