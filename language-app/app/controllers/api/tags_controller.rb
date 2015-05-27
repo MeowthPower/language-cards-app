691b62bf506117db2d9ab253ba1f460ece64617d
anna rankin <anna@annarankin.com>
2015-05-26 21:50:03 -0400

    created tags controller

--- language-app/app/controllers/api/tags_controller.rb
+++ language-app/app/controllers/api/tags_controller.rb
@@ -2,9 +2,15 @@ module Api
   class TagsController < ApplicationController
 
     def create
+      new_tag = Tag.new({card_id: params[:card_id],category_id: params[:category_id]})
+      new_tag.save
+      render json: new_tag.to_json
     end
 
     def destroy
+      poor_tag = Tag.find()
+      poor_tag.destroy
+      render json: poor_tag.to_json
     end
 
   end
