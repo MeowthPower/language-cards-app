53388f544e9695a19bc5636dbb3c487423724bd9
anna rankin <anna@annarankin.com>
2015-05-28 18:40:30 -0400

    sync

--- language-app/app/assets/javascripts/backbone/views/Cards/CardListView.js
+++ language-app/app/assets/javascripts/backbone/views/Cards/CardListView.js
@@ -3,20 +3,16 @@ var Meowth = Meowth || { Models: {}, Collections: {}, Views: {}, Routers: {} };
 Meowth.Views.CardListView = Backbone.View.extend({
   initialize: function(){
     this.listenTo(this.collection, 'add', this.addOne);
-
-    // this.render();
-  },
-
-  render: function(){
     var self = this;
     this.collection.each(function(card){ self.addOne(card) })
   },
-
   addOne: function(card){
     var cardView = new Meowth.Views.CardView({model: card});
     console.log("A card add event was fired")
     cardView.render();
-    // console.log(this.$el)
     this.$el.prepend(cardView.$el)
+  },
+  dispose: function(){
+    this.off()
   }
 })
